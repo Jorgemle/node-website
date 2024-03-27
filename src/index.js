6d087159ec895f2efc3da7c111b1acf2a6f8f8df
jorge M Linarez E <jorgemle24@gmail.com>
2024-03-27 08:25:40 -0400

    listo para produccion

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Bienvenido</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>Acerca de</h1>");
+});
+
+app.listen(process.env.PORT || 3000);
+console.log("server on port: ", process.env.PORT || 3000);
